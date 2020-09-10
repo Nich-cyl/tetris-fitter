9b99442b246dcfcbcd649e6b34488a158f40dad7
Neeeeech <leungnicholas121@gmail.com>
2020-09-10 16:10:10 +0100

    Created sol and allocate each 1 w/ color. Added color_dict to keep track of coords.

--- solve.py
+++ solve.py
@@ -1,5 +1,9 @@
 """SOLVES PROBLEM GRIDS"""
 
+def color_gen(seed):
+    return [(7*seed) % 256, (11*seed+50) % 256, (19*seed+100) % 256]
+
+
 def wrapper_for_each_in_grid(func):
     """runs func for all tiles, returning grid of outputs"""
     def function_wrapper(mat):
@@ -33,3 +37,25 @@ def wrapper_for_surrounding(func):
 @wrapper_for_surrounding
 def sum_surrounding(mat, i2, j2, i, j):
     return mat[i2][j2] if mat[i][j] != 0 else 0
+
+def sol(prbmat):
+    dim = len(prbmat)
+    cmpmat = [([0]*dim) for i in range(dim)]
+    color_dict = {}
+    num = 1
+    for i in range(dim):
+        for j in range(dim):
+            if prbmat[i][j] == 1:
+                cmpmat[i][j] = num
+                color_dict[num] = [i,j]
+                num += 1
+            else:
+                cmpmat[i][j] = 0
+    print(color_dict)
+    return cmpmat
+
+
+if __name__ == '__main__':
+    a = [[1,1,0],[1,0,0],[0,1,1]]
+    b = sol(a)
+    [print(i) for i in b]
